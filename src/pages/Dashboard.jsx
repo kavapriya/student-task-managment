65de84637f9112033bfb41b9010e160bc7c440ce
KavaPriya <kavapriya348@gmail.com>
2026-02-05 10:16:31 +0530

    [tasl is added]

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,39 +1,45 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import TaskList from "../components/TaskList";
-import { useEffect, useState } from "react";
 
 const Dashboard = () => {
   const navigate = useNavigate()
-  const [tasks,setTask]=useState([])
+  const[tasks, setTasks] = useState([])
 
-   useEffect(()=>{
+  useEffect(()=>{
     fetchData();
-   },[])
+  },[])
 
-  const fetchData = async()=>{
-    try{
+  const fetchData =async () => {
+    try {
       const response = await fetch("http://localhost:3000/tasks");
-      const data = response.json();
-      setTask(data);
-    }
-    catch(error){
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
       console.log(error)
-    };
-  }
-  const handleLogout=() => {
-  
+    }
+  };
+  useEffect(()=>{
+    console.log('called after API',tasks)
+  },[tasks])
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const handleLogout = () =>{
+    // console.log('click from dashboard')
     localStorage.removeItem('loginData')
     localStorage.removeItem('authData')
-    //localStorage.clear()
+    // localStorage.clear()
     navigate('/login')
   }
+
   return (
     <div>
-      <Navbar title="Task Management" onLogout={handleLogout}/>
+      <NavBar title="Task Management" onLogout={handleLogout}/>
       <h1>MY TASKS</h1>
-      <TaskList/>
-      <p>Welcome! You are logged in 🎉</p>
+      <TaskList tasks={tasks}/>
     </div>
   );
 };
